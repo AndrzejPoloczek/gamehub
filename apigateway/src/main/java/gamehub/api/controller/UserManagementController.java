54fcec50d0919d2f1262394b2595ae77ffdc4da3
AnsrzejPoloczek <andrzej.poloczek@gmail.com>
2020-02-05 15:53:17 +0100

    GH-11 implement full usermanager funcionslity in api

--- apigateway/src/main/java/gamehub/api/controller/UserManagementController.java
+++ apigateway/src/main/java/gamehub/api/controller/UserManagementController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import gamehub.api.clients.UserManagerClient;
 import gamehub.sdk.user.model.UserCreateDTO;
 import gamehub.sdk.user.model.UserInfoDTO;
+import gamehub.sdk.user.model.UserPasswordChangeDTO;
+import gamehub.sdk.user.model.UserUpdateDTO;
 
 
 @RestController
@@ -30,5 +32,20 @@ public class UserManagementController {
 	@GetMapping(path = "/get/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<UserInfoDTO> getByUsername(@PathVariable(required = true) String username) {
 		return userManagerClient.getByUsername(username);
+	}
+	
+	@PostMapping(path = "/update/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UserInfoDTO> update(@PathVariable String username, @RequestBody(required = true) UserUpdateDTO user) {
+		return userManagerClient.update(username, user);
+	}
+	
+	@PostMapping(path = "/changePassword/{username}")
+	public ResponseEntity<Object> changePassword(@PathVariable String username, @RequestBody(required = true) UserPasswordChangeDTO password) {
+		return userManagerClient.changePassword(username, password);
+	}
+	
+	@GetMapping(path = "/authorize/{username}/{password}")
+	public ResponseEntity<Boolean> authorize(@PathVariable(required = true) String username, @PathVariable(required = true) String password) {
+		return userManagerClient.authorize(username, password);
 	}
 }
