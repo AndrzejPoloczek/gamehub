d5f93e2e548c82dbfa93fe1dbd619c10c25213a1
AnsrzejPoloczek <andrzej.poloczek@gmail.com>
2020-03-11 10:15:18 +0100

    GH-23 Swagger documentation for api - DTOs documentation

--- gamehubsdk/src/main/java/gamehub/sdk/dto/gamebind/GameBindCheckDTO.java
+++ gamehubsdk/src/main/java/gamehub/sdk/dto/gamebind/GameBindCheckDTO.java
@@ -1,9 +1,18 @@
 package gamehub.sdk.dto.gamebind;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "Game bind check results")
 public class GameBindCheckDTO {
 
+    @ApiModelProperty(required = true, name = "Game bind ready flag", notes = "Inform, if game is ready to play")
     private boolean ready;
+
+    @ApiModelProperty(required = true, name = "Game bind canceled flag", notes = "Inform, if game bind has been canceled")
     private boolean canceled;
+
+    @ApiModelProperty(required = true, name = "Game play guid", notes = "Generated unique game play guid after successful binding")
     private String playGuid;
 
     public boolean isReady() {
--- gamehubsdk/src/main/java/gamehub/sdk/dto/user/UserInfoDTO.java
+++ gamehubsdk/src/main/java/gamehub/sdk/dto/user/UserInfoDTO.java
@@ -1,8 +1,15 @@
 package gamehub.sdk.dto.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "User information")
 public class UserInfoDTO {
 
+	@ApiModelProperty(required = true, name = "Display name", notes = "User's display name")
 	private String displayName;
+
+	@ApiModelProperty(required = true, name = "Username", notes = "User's username")
 	private String username;
 	
 	public String getDisplayName() {
