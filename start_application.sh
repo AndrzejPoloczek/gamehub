607476a6256d364d58d6f340235049aa2f1e4a25
AnsrzejPoloczek <andrzej.poloczek@gmail.com>
2020-03-23 12:47:28 +0100

    GH-24 Remove unnecessary file

--- start_application.sh
+++
@@ -1,5 +0,0 @@
-java -jar ./eureka/target/registration-and-discovery-service-0.0.1-SNAPSHOT.jar &
-java -jar ./usermanager/target/user-manager-service-0.0.1-SNAPSHOT.jar &
-java -jar ./gamebind/target/game-bind-service-0.0.1-SNAPSHOT.jar &
-java -jar ./gameplay/target/game-play-service-0.0.1-SNAPSHOT.jar &
-java -jar ./apigateway/target/api-gateway-service-0.0.1-SNAPSHOT.jar &
