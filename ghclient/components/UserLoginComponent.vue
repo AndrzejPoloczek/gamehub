c4c5edd8b054f688b93d77138c8cfa06f5d0a83f
AnsrzejPoloczek <andrzej.poloczek@gmail.com>
2020-04-14 08:50:46 +0200

    GH-32 Prepare vue/nuxt project

---
+++ ghclient/components/UserLoginComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="fv-padding fv-text-left">
+    <label class="fv-control-label fv-padding"> <i class="material-icons">person_add</i> Create New User </label>
+    <fvForm class="fv-row">
+      <fvFormElement class="fv-col-md-6" label="Username">
+        <fvInput v-model="userCreate.username" placeholder="Enter username" />
+      </fvFormElement>
+      <fvFormElement class="fv-col-md-6" label="Display name">
+        <fvInput v-model="userCreate.displayName" placeholder="Enter display name" />
+      </fvFormElement>
+      <fvFormElement class="fv-col-md-6" label="Password">
+        <fvInput type="password" v-model="userCreate.password" placeholder="Enter password" />
+      </fvFormElement>
+      <fvFormElement class="fv-col-md-6" label="Confirm password">
+        <fvInput type="password" v-model="userCreate.passwordRepeat" placeholder="Confirm password" />
+      </fvFormElement>
+      <fvFormElement class="fv-col-offset-only-sm-6" >
+        <fvButton class="fv-text-center fv-bg-primary">Create</fvButton>
+      </fvFormElement>
+    </fvForm>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "UseCreateComponent",
+        data() {
+          return {
+            userCreate: {
+              username: '',
+              displayName: '',
+              password: '',
+              passwordRepeat: ''
+            }
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ ghclient/pages/login.vue
@@ -0,0 +1,30 @@
+<template>
+  <fvMain>
+    <div class="content fv-padding">
+
+      <CurrentUserComponent />
+      <LoginComponent />
+
+      <div class="fv-padding fv-text-center fv-margin-top fv-margin-bottom">
+       ------ OR ------
+      </div>
+
+      <UseCreateComponent />
+    </div>
+  </fvMain>
+
+</template>
+
+<script>
+    import UseCreateComponent from "../components/UserLoginComponent";
+    import LoginComponent from "../components/LoginComponent";
+    import CurrentUserComponent from "../components/CurrentUserComponent";
+    export default {
+        name: "LoginPage",
+      components: {CurrentUserComponent, LoginComponent, UseCreateComponent}
+    }
+</script>
+
+<style scoped>
+
+</style>
